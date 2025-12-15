6fc57bb106efbdb17a58bbdfe0ad835443c13049
firedHouse <y.hyungu4704@gmail.com>
2025-12-15 10:59:58 +0900

    [feat] 폴더링 작업 및 Singleton 뼈대 클래스 제작
    기본 폴더링 작업 6가지
    
    Singleton 제네릭 클래스 제작

---
+++ Assets/Scripts/Manager/Singleton.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+// 각종 매니저 상속용 제네릭 기반 클래스
+// MonoBehaviour 상속받는 클래스로 제한
+public class Singleton<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _instance;
+
+    //외부 호출용 프로퍼티, 해당 타입의 싱글톤이 없으면 찾아보고, 없을 시 새로 생성 후 설정
+    public static T Instance
+    {
+        get
+        {
+            return _instance;
+        }
+    }
+    //중복체크 및 연결 기능 구현
+    protected virtual void Awake()
+    {
+        if (_instance == null)
+        {
+            _instance = this as T;
+            DontDestroyOnLoad(gameObject);
+        }
+        //이미 인스턴스 있고, 서로 다른 경우 (중복)
+        //원본 아니니까(이미 있으니까) 파괴
+        else
+        {
+            if (_instance != this)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}

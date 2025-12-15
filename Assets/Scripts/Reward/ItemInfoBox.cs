28bb810f9975a715d828e6272c2f6ab33216fd97
gpwn145 <gpwn145@gmail.com>
2025-12-15 18:30:27 +0900

    [feat] 스테이지 클리어 보상 아이템 화면
    1. 전체 레이아웃 배치('Hyeju_TestScene')
      - 프리팹 생성(Hyeju_ItemRewardPrefab) : 'GoldInfo', 'ItemBox', 'ItemInfoBox', 'NextStageButton', 'StageInfo'
    2. 아이템 마우스 오버 : 아이템 정보 텍스트 표시
    3. 아이템 클릭 : 골드 차감, 캐릭터 선택 팝업 표시 (닫기X)
    4. 작성 Script (Reward) : 'GoldInfo', 'ItemInfoBox', 'NextStageButton',
    5. 테스트 Script 작성('Hyeju_(Test)Script') : 'TestPresenter', 'GoldModelScript)
    
    - 아이템별 정보 텍스트 연결  X
    - 스테이지 정보 연결 X
    - 다음 스테이지 버튼 연결 X :

---
+++ Assets/Scripts/Reward/ItemInfoBox.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ItemInfo : MonoBehaviour
+{
+    [SerializeField] private Text _itemInfoText;
+    [SerializeField] private GameObject _ItemUsecharacterChoicePanel;
+    [SerializeField] private GoldModelScript _goldModelScript;
+
+
+    public void OnPointOver()
+    {
+        _itemInfoText.text = "아이템 설명";
+    }
+
+    public void OnPointExit()
+    {
+        _itemInfoText.text = "비어있음";
+    }
+
+    public void OnItemClick()
+    {
+        // 아이템 구매
+        // 골드 감소(아이템 가격만큼)
+        _goldModelScript.SpendGold(1000);
+
+        // 사용할 캐릭터 팝업 출력
+        _ItemUsecharacterChoicePanel.SetActive(true);
+    }
+}

33ce0c7dad6016b0545821e6c3d7c6f9d40dfd24
gpwn145 <gpwn145@gmail.com>
2025-12-17 16:00:01 +0900

    [Refactor] 스크립트 수정
    MouseOverInfo.cs 수정

--- Assets/Hyeju_Test/Hyeju_(Test)Script/Battle/MouseOverInfo.cs
+++ Assets/Hyeju_Test/Hyeju_(Test)Script/Battle/MouseOverInfo.cs
@@ -1,45 +1,44 @@
+using UnityEditor.U2D.Animation;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class MouseOverInfo : MonoBehaviour
 {
-    [SerializeField] private GameObject _speedAttackInfoPanel;
+    //스킬 정보표시할 텍스트
     [SerializeField] private Text _skillInfo;
-    [SerializeField] private CharacterSkill _characterSkill;
-    [SerializeField] private SkillData[] _useSkillList = new SkillData[3];
+    //받아온 스킬 데이터 스크립트
+    private CharacterData _characterData;
 
+
+    //스킬 정보 받아오기 
     private void Start()
     {
-        _useSkillList[0] = _characterSkill.TestSkill1;
+        //캐릭터id에 해당하는 스킬을 불러와야 함.
+
     }
 
-    public void OnCharacterOver()
-    {
-        _speedAttackInfoPanel.SetActive(true);
-    }
-
-    public void OnCharacterExit()
-    {
-        _speedAttackInfoPanel.SetActive(false);
-    }
-
-    public void OnFirSkillInfo()
-    {
-        _skillInfo.text = $"{_useSkillList[0].skillName}";
-    }
-
+    //공통 : 해당 캐릭터의 첫번째 스킬이 기본적으로 출력된다.
     public void OnSkillExit()
     {
-        _skillInfo.text = $"{_useSkillList[0].skillName}";
-    }
-    public void OnSecSkillInfo()
-    {
-        _skillInfo.text = $"2번 스킬";
+        _skillInfo.text = $"{_characterData.ownedSkill01}";
     }
 
+    //1번스킬 출력
+    public void OnFirSkillInfo()
+    {
+        _skillInfo.text = $"{_characterData.ownedSkill01}";
+    }
+
+    //2번스킬 출력
+    public void OnSecSkillInfo()
+    {
+        _skillInfo.text = $"{_characterData.ownedSkill02}";
+    }
+
+    //3번스킬 출력
     public void OnThirSkillInfo()
     {
-        _skillInfo.text = $"3번 스킬";
+        _skillInfo.text = $"{_characterData.ownedSkill03}";
     }
 
 }

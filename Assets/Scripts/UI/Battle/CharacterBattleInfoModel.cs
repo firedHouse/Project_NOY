7aaa41726f51ff830adc8ba8f764e30aa950f43a
jeonjyu <jihoojeon77@gmail.com>
2025-12-19 15:48:40 +0900

    [Fix] 머지시 충돌로 인한 코드 수정

--- Assets/Scripts/UI/Battle/CharacterBattleInfoModel.cs
+++ Assets/Scripts/UI/Battle/CharacterBattleInfoModel.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 public class CharacterBattleInfoModel : MonoBehaviour
 {
     [SerializeField] private string _characterId = "character_id_10001";
-    [SerializeField] public CharacterData character;
+    //[SerializeField] public CharacterData character;
     //Hyeju : 테스트용 데이터 배열
     //[SerializeField] public CharacterData[] testCharacter = new CharacterData[3];
     [SerializeField] private UnitPosition position = UnitPosition.Front;
@@ -17,13 +17,12 @@ public class CharacterBattleInfoModel : MonoBehaviour
     //Hyeju : 캐릭터 사망 이벤트 추가
     public event Action<string> Death;
 
-    public CharacterData Character { get => character; set => character = value; }
-    public string CharacterName { get => character.characterName; }
+    //public CharacterData Character { get => character; set => character = value; }
+    //public string CharacterName { get => character.characterName; }
     public float MaxHP { get => maxHP; set => maxHP = value; }
 
     private void Start()
     {
-        character = TableManager.Instance.CharacterTable.Get(_characterId);
         //Hyeju
         // testCharacter[0] = character;
         // testCharacter[1] = TableManager.Instance.CharacterTable.Get("character_id_10002");
@@ -54,10 +53,9 @@ public class CharacterBattleInfoModel : MonoBehaviour
     //    character.HPLevel1 -= amount;
     //    character.HPLevel1 = Mathf.Clamp(character.HPLevel1, 0, maxHP);
 
-        HPChanged?.Invoke();
         //hyeju 
         //Die();
-    }
+    //}
 
     //Hyeju : 캐릭터의 포지션 값을 전달
     // public void Die()

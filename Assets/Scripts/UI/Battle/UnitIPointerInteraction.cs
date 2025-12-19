5c6be45f18e9b77746ff6db12455f30f9b6f958d
jeonjyu <jihoojeon77@gmail.com>
2025-12-19 15:50:22 +0900

    [Feat] 터치 기능 추가

---
+++ Assets/Scripts/UI/Battle/UnitIPointerInteraction.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class UnitIPointerInteraction : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField] private Skill skill;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        Debug.Log($"[UnitIPointerInteraction] 클릭됨");
+        //skill.TryUse();
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

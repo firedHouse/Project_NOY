aed6ccac12657fafc8a09282e5ae239711a5dd7d
gpwn145 <gpwn145@gmail.com>
2025-12-19 12:10:23 +0900

    Docs: 스킬 프리팹, 코드 수정

--- Assets/Hyeju_Test/Hyeju_(Test)Script/Battle/MouseOverInfo.cs
+++ Assets/Hyeju_Test/Hyeju_(Test)Script/Battle/MouseOverInfo.cs
@@ -1,4 +1,4 @@
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 //view
@@ -13,25 +13,14 @@ public class MouseOverInfo : MonoBehaviour
     private Image _ImageInfo;
 
 
-    //캐릭터 순서 변화 감지 시 스킬 이미지 변경
-    //character 정보가 변경되었을 때 이미지도 바뀌어야 함.
-    //캐릭터별 턴이 넘어가려면 : 전열 스킬 선택 > 중열 스킬 선택 > 후열스킬 선택
-
-    // >> 스킬 클릭 시 <<
-    //혹은
-    // >> 스킬 순서를 저장하는 스택이 null 이 아닐때. <<
-    //혹은
-    // >> 그냥 턴 수를 int로 저장 <<
-
     #region 마우스오버
-    //공통 : 해당 캐릭터의 첫번째 스킬이 기본적으로 출력된다.
-    public void OnSkillExit()
+    //1번스킬 출력
+    public void OnFirSkillInfo()
     {
         _skillInfo.text = $"{_skillList._characterSkill[0]?.skillName}";
     }
 
-    //1번스킬 출력
-    public void OnFirSkillInfo()
+    public void OnFirSkillExit()
     {
         _skillInfo.text = $"{_skillList._characterSkill[0]?.skillName}";
     }
@@ -42,8 +31,18 @@ public class MouseOverInfo : MonoBehaviour
         _skillInfo.text = $"{_skillList._characterSkill[1]?.skillName}";
     }
 
+    public void OnSecSkillExit()
+    {
+        _skillInfo.text = $"{_skillList._characterSkill[1]?.skillName}";
+    }
+
     //3번스킬 출력
     public void OnThirSkillInfo()
+    {
+        _skillInfo.text = $"{_skillList._characterSkill[2]?.skillName}";
+    }
+
+    public void OnThirSkillExit()
     {
         _skillInfo.text = $"{_skillList._characterSkill[2]?.skillName}";
     }

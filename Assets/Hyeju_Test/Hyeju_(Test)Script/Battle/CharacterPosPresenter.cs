a1cc334eb462a4b3b6dd24f26ca5d4e91b911c07
gpwn145 <gpwn145@gmail.com>
2025-12-20 06:36:52 +0900

    [Refactor] 사망 시 캐릭터 이동처리

--- Assets/Hyeju_Test/Hyeju_(Test)Script/Battle/CharacterPosPresenter.cs
+++ Assets/Hyeju_Test/Hyeju_(Test)Script/Battle/CharacterPosPresenter.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.TextCore.Text;
+using static UnityEditor.PlayerSettings;
 
 public class CharacterPosPresenter : MonoBehaviour
 {
@@ -17,34 +19,58 @@ public class CharacterPosPresenter : MonoBehaviour
 
     private List<Character> PlayerTeam = new List<Character>();
 
-    private UnitPosition position;
+    [SerializeField] public GameObject[] positionInit = new GameObject[3];
+
 
     //BattleUnit.MovePosition(UnitPosition newPosition)
 
     //모델 정보 받아서 뷰에 전달해야 함.
     private void Start()
     {
-        //팀 리스트
-        PlayerTeam = BattleManager.Instance.PlayerTeam;
+
     }
 
-    public void SurvivalList()
+    public void DeathCharacter()
     {
+        //팀 리스트
+        PlayerTeam = BattleManager.Instance.PlayerTeam;
+        Debug.Log($"{PlayerTeam[0].name}");
+
         // 데이터가 없으면 리턴
         if (PlayerTeam == null)
         {
+            Debug.Log("[CharacterPosPresenter] : 팀없음");
             return;
         }
 
-        for(int i = 0; i < 3; i++)
+        for (int i = 0; i < PlayerTeam.Count; i++)
         {
             if (PlayerTeam[i].IsDead == true)
             {
+                Debug.Log("[CharacterPosPresenter] : 사망자 발생");
                 BattleManager.Instance.OnUnitDead(PlayerTeam[i]);
-            }
+                _view.IsEnpty(PlayerTeam[i]);
 
+                //뷰에서는 캐릭터 정보도 비활성화 해야함
+                _view.Inactive();
+            }
         }
+
+        AliveCharecter();
     }
 
+    public void AliveCharecter()
+    {
+        for (int i = 0; i < PlayerTeam.Count; i++)
+        {
+            if (PlayerTeam[i].IsDead == false)
+            {
+                //위치 갱신
+                _view.ReSetPosition(PlayerTeam[i], positionInit);
+                Debug.Log("[CharacterPosPresenter] : 생존캐릭터 위치 세팅");
+            }
+        }
+    }
 }
+
 
--- Assets/Hyeju_Test/Hyeju_(Test)Script/Battle/CharacterPositionView.cs
+++ Assets/Hyeju_Test/Hyeju_(Test)Script/Battle/CharacterPositionView.cs
@@ -3,6 +3,7 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.TextCore.Text;
 using UnityEngine.UI;
+using UnityEngine.UIElements;
 
 public class CharacterPositionView : MonoBehaviour
 {
@@ -10,24 +11,36 @@ public class CharacterPositionView : MonoBehaviour
 
     //화면에 표시될 박스
     [SerializeField] private GameObject[] _characterBox = new GameObject[3];
+    bool[] isEnpty = new bool[3];
 
+    //사망 캐릭터 박스 비활성화
+    public void Inactive()
+    {
+        //칸 비활성화
+        if (_characterBox[2].activeSelf == true)
+        {
+            _characterBox[2].SetActive(false);
+            Debug.Log($"[CharacterPositionView] : 후열 비활성화");
+            return;
+        }
+        if (_characterBox[1].activeSelf == true)
+        {
+            _characterBox[1].SetActive(false);
+            Debug.Log($"[CharacterPositionView] : 중열 비활성화");
+            return;
+        }
+        if (_characterBox[0].activeSelf == true)
+        {
+            _characterBox[0].SetActive(false);
+            Debug.Log($"[CharacterPositionView] : 전열 비활성화");
+        }
+    }
 
-    
-
-    //칸 전체 초기화 : 노출된 정보 전체 제거
-    //사망한 캐릭터만 모아서 호출해도 될 것 같음.
-    //public void InitBox(List<(Character, int)> DeatList)
-    //{
-    //    // 2번칸부터 DeatList.Count 수 만큼 반복
-    //    int cycle = 3 - DeatList.Count;
-
-    //    for (int i = 2; i >= cycle; i--)
-    //    {
-    //        //칸 비활성화
-    //        _characterBox[i].SetActive(false);
-    //        DeatList[i].Item1.gameObject.SetActive(false);
-    //    }
-    //}
+    public void IsEnpty(Character deathChar)
+    {
+        isEnpty[(int)deathChar.Position - 1] = true;
+        Debug.Log($"[CharacterPositionView] : 포지션 이넘값{(int)deathChar.Position - 1}");
+    }
 
     //3번 칸부터 데이터 작성
     //[2] = 왼쪽칸
@@ -39,32 +52,7 @@ public class CharacterPositionView : MonoBehaviour
     // 2. UI 이미지가 이동
 
     //체력 닳을때 체크 (캐릭터 체력 구독, 실행해야 함.)
-    //public void ReSetPosition(List<(Character, int)> aliveList)
-    //{
-    //    bool[] isEnpty = new bool[3];
-    //    for(int i = 0; i > 3; i++)
-    //    {
-    //        switch(aliveList[i].Item2)
-    //        {
-    //            //전열 : 앞으로 이동하지 않는다.
-    //            case 0:
-    //                isEnpty[0] = false;
-    //                break;
-    //            //중앙 : 앞에 비어있지 않으면 앞으로 이동
-    //            case 1:
-    //                if (isEnpty[0] == true)
-    //                {
-    //                    aliveList[i].Item1.Position = 
-    //                }
-    //                break;
-    //            //후열 : 앞에 빈만큼 이동
-    //            case 2:
-    //                break;
-    //        }
-
-    //        if(aliveList[i].Item2 == 1 && )
-
-    //            Debug.Log($"[CharacterPosition] : 위치 재설정");
-    //    }
-    //}
+    public void ReSetPosition(Character character, GameObject[] pos)
+    {
+    }
 }

c5016e7d4779091add07380b5c7e51bcf138f40c
gpwn145 <gpwn145@gmail.com>
2025-12-23 15:04:20 +0900

    [feat] 캐릭터 성장 UI
    - GrowthPresenter
    - GrowthView

---
+++ Assets/Scripts/UI/Grade/GrowthPresenter.cs
@@ -0,0 +1,87 @@
+﻿using UnityEngine;
+
+public class GrowthPresenter : MonoBehaviour
+{
+    [SerializeField] private GrowthView view;
+    //[SerializeField] private Character model;
+    [SerializeField] private CharacterData model;
+
+    //임시 변수 : 캐릭터 해금 상태에서 가져와야 함.
+    private bool IsUnlock = false;
+    private float currentShilling = 0;
+    private float upgradeCost;
+
+    //업그레이드 성공 여부 > 모델에서 가져가면 되지 않을까/ 레벨 1회 상승 후 false
+    private bool isUpgrade = false;
+    public bool IsUpgrade { get { return isUpgrade; } private set { isUpgrade = value; } }
+
+    //성장 버튼 클릭 활성화 조건 : 캐릭터 해금
+    //버튼 클릭 시 : 실링 확인
+    //부족 > 실링부족 패널
+
+    //충분 > 성장 > 학년증가(모델에 전달) > 회색별 > 노란별
+    //성장 후 업데이트 사항
+    //학년 증가 : 모델에서 할 일
+    //다음 성장 필요 실링 출력
+
+    //최고 학년 > 성장버튼 클릭 비활성화
+
+    private void Start()
+    {
+        //testModel, 출력대상 : 클릭한 캐릭터
+        model = TableManager.Instance.CharacterTable.Get("10001");
+        //성장이벤트 구독 
+        //별갱신 : model.성장시 발동 이벤트 += view.GradeSet(grade)
+        //비용갱신 : model.성장시 발동 이벤트 += view.UpgradeCost(grade)
+        //캐릭터해금 : model.해금시 발동 이벤트 += view.CanClick()
+
+        Init();
+    }
+    private void Init()
+    {
+        //임시 속성값
+        //모델에서 고유속성 불러오기
+        view.CharacterElement(0);
+        //버튼클릭 비활성화
+        view.ButtonActive(false);
+        //별 이미지 세팅(0:노란별 / 1,2:회색별)
+        view.GradeSet(1);
+        view.CharacterInfo(model.characterName, model.characterCodeName);
+        view.CharacterInfo(model.characterInfo);
+    }
+
+
+    public void SuccessUpgrade()
+    {
+        isUpgrade = true;
+    }
+
+    //캐릭터가 해금 상태이면, 버튼 활성화
+    public void CanClick()
+    {
+        if(IsUnlock == true)
+        {
+            view.ButtonActive(true);
+        }
+    }
+
+    //보유 실링 체크 : 구매 여부 체크
+    public bool IsCanUpgrade()
+    {
+        if(currentShilling < upgradeCost)
+        {
+            view.OnNotEnoughShilling(true);
+            return true;
+        }
+        return false;
+    }
+
+    //최고 학년 체크
+    public void MaxLevel()
+    {
+        //if(model.Level == 3)
+        //{
+        //    view.ButtonActive(false);
+        //}
+    }
+}
---
+++ Assets/Scripts/UI/Grade/GrowthView.cs
@@ -0,0 +1,142 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class GrowthView : MonoBehaviour
+{
+    //캐릭터의 학년 정보 받아와야 함.
+    [Header("임시변수")]
+    [SerializeField] private int grade;
+
+    [Header("실링부족텍스트")]
+    [SerializeField] private Text notEnoughShilingPanel;
+
+    [Header("닫기버튼")]
+    [SerializeField] private Text OKPanel;
+
+    [Header("성장버튼/텍스트")]
+    [SerializeField] private Button upgradeButton;
+    [SerializeField] private Text upgradeCostText;
+
+    [Header("GrowthPresenter")]
+    [SerializeField] private GrowthPresenter presenter;
+
+    [Header("별 출력 배열/리소스")]
+    [SerializeField] private Image[] starImage = new Image[3];
+    [SerializeField] private Sprite yellowStar;
+    [SerializeField] private Sprite grayStar;
+
+    [Header("고유속성 출력 배열/리소스(물/불/번개/무속)")]
+    [SerializeField] private Image elementImage;
+    [SerializeField] private Sprite[] elementImageResources = new Sprite[4];
+
+    [Header("캐릭터 이름/코드네임/캐릭터 설명")]
+    [SerializeField] private Text charName;
+    [SerializeField] private Text codeName;
+    [SerializeField] private Text infoText;
+
+
+
+    //재사용 안된다면 삭제
+    #region 실링 부족 경고창
+    private void NotEnoughShiling ()
+    {
+        notEnoughShilingPanel.text = "실링이 부족합니다.";
+    }
+
+    private void OKText ()
+    {
+        OKPanel.text = "인정";
+    }
+    #endregion
+   
+    //성장버튼 클릭 액션
+    public void OnClickUpgradeButton()
+    {
+        //실링이 충분함
+        if(presenter.IsCanUpgrade() == true)
+        {
+            //업그레이드 정보 전달
+            presenter.SuccessUpgrade();
+        }
+        //실링이 부족함
+        else if(presenter.IsCanUpgrade() == false)
+        {
+            //실링부족 > 패널 띄움
+            OnNotEnoughShilling(true);
+        }
+    }
+
+    //확인 버튼 클릭 액션
+    public void OnOKButton()
+    {
+        //패널 닫기
+        OnNotEnoughShilling(false);
+    }
+
+    //고유속성 출력 : ElementUI elementUI
+    //0불 /1물 / 2번개 / 3무속성
+    public void CharacterElement(int element)
+    {
+        elementImage.sprite = elementImageResources[element];
+    }
+
+    //이름, 코드네임
+    public void CharacterInfo(string name, string _codeName)
+    {
+        charName.text = name;
+        codeName.text = _codeName;
+    }
+
+    //세부설정
+    public void CharacterInfo(string info)
+    {
+        infoText.text = info;
+    }
+
+    //비용 업데이트
+    public void UpgradeCost(int grade)
+    {
+        upgradeCostText.text = $"{grade * 1000}";
+    }
+
+
+    
+
+    //버튼 활성/비활성
+    public void ButtonActive(bool canClick)
+    {
+        upgradeButton.interactable = canClick;
+    }
+
+    //실링 부족 경고창
+    public void OnNotEnoughShilling(bool isBlocked)
+    {
+        upgradeButton.interactable = isBlocked;
+    }
+
+    //별 이미지 갱신
+    public void GradeSet(int grade)
+    {
+        if(yellowStar == null || grayStar ==null)
+        {
+            Debug.Log("[GrowthView] 별 이미지 정보 없음");
+            return;
+        }
+        if (grade > 3)
+        {
+            Debug.Log("[GrowthView] 등급 최대치 넘어감");
+            return;
+        }
+
+        //등급만큼 노란별
+        for (int i = 0; i < grade; i++)
+        {
+            starImage[i].sprite = yellowStar;
+        }
+        //
+        for (int i = grade; i < 3; i++)
+        {
+            starImage[i].sprite = grayStar;
+        }
+    }
+}

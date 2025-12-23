352663ddca620cdd70607a9371330cd6163bcb9a
firedHouse <y.hyungu4704@gmail.com>
2025-12-23 23:03:21 +0900

    [feat] 턴 정산 로직 추가
    과부하, 버프. 디버프에서 활용할 턴 정산 로직 구현

---
+++ Assets/Scripts/Battle/States/StateTurnEnd.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using UnityEngine;
+
+//버프/디버프 지속시간 감소와 과부하 처리할 상태
+public class StateTurnEnd : IBattleState
+{
+    //정산끝
+    private bool isFinished = false;
+    public void Enter(BattleManager bm)
+    {
+        isFinished = false;
+        bm.StartCoroutine(ProcessTurnEnd(bm));
+    }
+    public void Execute(BattleManager bm)
+    {
+        if (isFinished)
+        {
+            // 승패 체크 후 다음 턴으로 넘기기
+            if (CheckWinLoss(bm))
+            {
+                return;
+            }
+            //정산 종료 이후 다시 플레이어 턴 시작
+            Debug.Log("정산 완료 -> 다음 턴 시작");
+            bm.NotifyPlayerTurnStart(); 
+            bm.ChangeState(new StatePlayerTurn());
+        }
+    }
+    public void Exit(BattleManager bm)
+    {
+
+    }
+
+    //턴종료 프로세스(유닛이 각자 처리하도록 메서드 추가)
+    private IEnumerator ProcessTurnEnd(BattleManager bm)
+    {
+        Debug.Log("턴 정산 시작");
+        //아군
+        foreach (var unit in bm.PlayerTeam)
+        {
+            if (!unit.IsDead)
+            {
+                unit.OnTurnEnd(bm.PlayerTeam);
+            }
+        }
+        //적군
+        foreach (var unit in bm.EnemyTeam)
+        {
+            if (!unit.IsDead)
+            {
+                unit.OnTurnEnd(bm.EnemyTeam);
+            }
+        }
+
+        yield return new WaitForSeconds(1.5f);
+        isFinished = true;
+    }
+
+    private bool CheckWinLoss(BattleManager bm)
+    {
+        if (bm.EnemyTeam.Count == 0)
+        {
+            bm.ChangeState(new StateEnd(true));
+            return true;
+        }
+        if (bm.PlayerTeam.Count == 0)
+        {
+            bm.ChangeState(new StateEnd(false));
+            return true;
+        }
+        return false;
+    }
+}

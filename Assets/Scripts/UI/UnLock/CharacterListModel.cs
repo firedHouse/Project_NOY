131022bd0e8ff47d6c21b216772ca2d4b68453c9
gpwn145 <gpwn145@gmail.com>
2025-12-26 17:39:19 +0900

    [feat] 해금 시 이벤트 (invoke추가)
    - CharacterListModel

--- Assets/Scripts/UI/UnLock/CharacterListModel.cs
+++ Assets/Scripts/UI/UnLock/CharacterListModel.cs
@@ -78,8 +78,15 @@ public partial class CharacterListModel : MonoBehaviour
             Debug.Log("캐릭터 데이터 불러오기 실패");
         }
 
+        presenter = GameObject.Find("CharacterListPanel").GetComponent<CharacterListPresenter>();
+        if(presenter == null)
+        {
+            Debug.Log("[CharacterListModel] 프레젠터없음");
+
+            return;
+        }
+
         presenter.GetGradeData();
-        SetNeedShilling();
         // 스킬 로드 (BattelUnit.LoadSkills 메서드 사용 예정)> 팀 구성에서 이 클래스 사용하게 되면 추가
     }
 
@@ -93,6 +100,7 @@ public partial class CharacterListModel : MonoBehaviour
         {
             Debug.Log($"[CharacterListModel] {isUnlocked} > 해금");
             isUnlocked = true;
+            OnUnlock.Invoke(this);
         }
         else
         {

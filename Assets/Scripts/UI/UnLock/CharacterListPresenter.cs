0a53021853fb2882318dbb6c6ede62c00db75b59
gpwn145 <gpwn145@gmail.com>
2025-12-26 17:38:43 +0900

    [fix] 금액 갱신 안되는 버그(프레젠터)
    - CharacterListModel

--- Assets/Scripts/UI/UnLock/CharacterListPresenter.cs
+++ Assets/Scripts/UI/UnLock/CharacterListPresenter.cs
@@ -1,6 +1,7 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
-using System.Collections.Generic;
+using UnityEngine.TextCore.Text;
 using UnityEngine.UI;
 
 // 캐릭터 목록에서 캐릭터 정보 출력에 전체적으로 사용할 프레젠터
@@ -37,8 +38,9 @@ public partial class CharacterListPresenter : MonoBehaviour
         // 초기 설정으로 첫 슬롯 캐릭터 지정해주기
         ShowDetailView(characterSlots[0].gameObject.GetComponent<CharacterListModel>());
         // Hyeju
-        Init();
+        Init(characterSlots[0].gameObject.GetComponent<CharacterListModel>());
         model.OnUnlock += CanClick;
+        model.OnUnlock += growthView.CharacterInfo;
         model.OnUpgrade += UpdateCharacterInfo;
     }
 

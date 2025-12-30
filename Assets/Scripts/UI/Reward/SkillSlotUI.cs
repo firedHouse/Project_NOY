7f1fe163550f4e331ec7c67a7ffc357b92378342
ChocoCider <hsj183040@gmail.com>
2025-12-30 17:50:31 +0900

    [Feat] SkillSlotUI 하나 제작 및 SkillSelect에 있는 스킬 설명에 이벤트 형식으로 제작
    스킬 슬롯에 마우스를 올리면 스킬 이름 및 설명이 뜨도록 수정함

---
+++ Assets/Scripts/UI/Reward/SkillSlotUI.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System;
+
+public class SkillSlotUI : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    [SerializeField] private Button button;
+    [SerializeField] private Image icon;
+    [SerializeField] private Text ppText;
+
+    private Skill skill;
+    private Action<Skill> onClick;
+    private Action<Skill> onHover;
+    private Action onExit;
+
+    public void Set(
+        Skill skill,
+        Action<Skill> onClick,
+        Action<Skill> onHover,
+        Action onExit)
+    {
+        this.skill = skill;
+        this.onClick = onClick;
+        this.onHover = onHover;
+        this.onExit = onExit;
+
+        icon.sprite = ResourceManager.Instance.LoadSprite(skill.Data.skillIcon);
+        ppText.text = $"PP {skill.CurrentPP}/{skill.Data.skillPP}";
+        button.interactable = skill.CurrentPP > 0;
+
+        button.onClick.RemoveAllListeners();
+        button.onClick.AddListener(() => this.onClick?.Invoke(skill));
+
+        gameObject.SetActive(true);
+    }
+
+    public void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        onHover?.Invoke(skill);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        onExit?.Invoke();
+    }
+}
